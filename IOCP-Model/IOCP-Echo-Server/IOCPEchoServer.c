5265810bb6b99df87d2cb6dc7e5b2d7ba2aa9579
hyun <ahn020367@gmail.com>
2023-08-26 23:24:13 +0900

    IOCP Echo Server 예제 작성

--- IOCP-Model/IOCP-Echo-Server/IOCPEchoServer.c
+++ IOCP-Model/IOCP-Echo-Server/IOCPEchoServer.c
@@ -22,3 +22,143 @@ CompletionPort에 할당되는 쓰레드의 의미
 	* CP 오브젝트에 할당가능 한 쓰레드의 수를 2개로 제한하는 것은 GetQueuedCompletionStatus 함수를 동시에 호출할 수 있는 쓰레드의 수를 2개로 제한한다는 것
 	* 결국 2개의 쓰레드만 CP 오브젝트에서 일할 수 있다는 것
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <process.h>
+#include <winsock2.h>
+#include <windows.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define BUF_SIZE 100
+#define READ	3
+#define	WRITE	5
+
+typedef struct    // socket info
+{
+	SOCKET hClntSock;
+	SOCKADDR_IN clntAdr;
+} PER_HANDLE_DATA, * LPPER_HANDLE_DATA;
+
+typedef struct    // buffer info
+{
+	OVERLAPPED overlapped;
+	WSABUF wsaBuf;
+	char buffer[BUF_SIZE];
+	int rwMode;    // READ or WRITE
+} PER_IO_DATA, * LPPER_IO_DATA;
+
+DWORD WINAPI EchoThreadMain(LPVOID CompletionPortIO);
+void ErrorHandling(char* message);
+
+int main()
+{
+	WSADATA	wsaData;
+	HANDLE hComPort;
+	SYSTEM_INFO sysInfo;
+	LPPER_IO_DATA ioInfo;
+	LPPER_HANDLE_DATA handleInfo;
+
+	SOCKET hServSock;
+	SOCKADDR_IN servAdr;
+	int recvBytes, i, flags = 0;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		ErrorHandling("WSAStartup() error!");
+
+	hComPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
+	GetSystemInfo(&sysInfo);
+
+	for (i = 0; i < sysInfo.dwNumberOfProcessors; i++)
+		_beginthreadex(NULL, 0, EchoThreadMain, (LPVOID)hComPort, 0, NULL);
+
+	hServSock = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+	memset(&servAdr, 0, sizeof(servAdr));
+	servAdr.sin_family = AF_INET;
+	servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servAdr.sin_port = htons(atoi("8080"));
+
+	bind(hServSock, (SOCKADDR*)&servAdr, sizeof(servAdr));
+	listen(hServSock, 5);
+
+	while (1)
+	{
+		SOCKET hClntSock;
+		SOCKADDR_IN clntAdr;
+		int addrLen = sizeof(clntAdr);
+
+		hClntSock = accept(hServSock, (SOCKADDR*)&clntAdr, &addrLen);
+
+		handleInfo = (LPPER_HANDLE_DATA)malloc(sizeof(PER_HANDLE_DATA));
+		handleInfo->hClntSock = hClntSock;
+		memcpy(&(handleInfo->clntAdr), &clntAdr, addrLen);
+
+		CreateIoCompletionPort((HANDLE)hClntSock, hComPort, (DWORD)handleInfo, 0);
+
+		ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
+		memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
+		ioInfo->wsaBuf.len = BUF_SIZE;
+		ioInfo->wsaBuf.buf = ioInfo->buffer;
+		ioInfo->rwMode = READ;
+
+		WSARecv(handleInfo->hClntSock, &(ioInfo->wsaBuf),
+			1, &recvBytes, &flags, &(ioInfo->overlapped), NULL);
+	}
+	return 0;
+}
+
+DWORD WINAPI EchoThreadMain(LPVOID pComPort)
+{
+	HANDLE hComPort = (HANDLE)pComPort;
+	SOCKET sock;
+	DWORD bytesTrans;
+	LPPER_HANDLE_DATA handleInfo;
+	LPPER_IO_DATA ioInfo;
+	DWORD flags = 0;
+
+	while (1)
+	{
+		GetQueuedCompletionStatus(hComPort, &bytesTrans,
+			(LPDWORD)&handleInfo, (LPOVERLAPPED*)&ioInfo, INFINITE);
+		sock = handleInfo->hClntSock;
+
+		if (ioInfo->rwMode == READ)
+		{
+			puts("message received!");
+			if (bytesTrans == 0)    // EOF 전송 시
+			{
+				closesocket(sock);
+				free(handleInfo); free(ioInfo);
+				continue;
+			}
+
+			memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
+			ioInfo->wsaBuf.len = bytesTrans;
+			ioInfo->rwMode = WRITE;
+			WSASend(sock, &(ioInfo->wsaBuf),
+				1, NULL, 0, &(ioInfo->overlapped), NULL);
+
+			ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
+			memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
+			ioInfo->wsaBuf.len = BUF_SIZE;
+			ioInfo->wsaBuf.buf = ioInfo->buffer;
+			ioInfo->rwMode = READ;
+			WSARecv(sock, &(ioInfo->wsaBuf),
+				1, NULL, &flags, &(ioInfo->overlapped), NULL);
+		}
+		else
+		{
+			puts("message sent!");
+			free(ioInfo);
+		}
+	}
+	return 0;
+}
+
+void ErrorHandling(char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
